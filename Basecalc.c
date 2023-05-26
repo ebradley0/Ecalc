2a1196fa65effd9d13aa089e080217894156c48a
ebradley0 <ebradley842@gmail.com>
2023-05-25 22:11:52 -0400

    Changed functions from numbers to type, reformatted to look a little cleaner. Broken up to multiple functions between identifiying type and doing math.

--- Basecalc.c
+++ Basecalc.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef enum
+{
+    ADDITION = 1,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION,
+} function_t;
+
+function_t arg_to_function(char *arg);
+
 int main(int argc, char *argv[])
 {
-    int i; // for loop
+    unsigned int i; // for loop
     int p;
     char g; // Function type reference
     char b;
@@ -14,57 +25,23 @@ int main(int argc, char *argv[])
     int debounce = 0;
     double results;
     double storage[100];
-    int function = 0;
+    char my_string[10];
+
     printf("Input: %s\n", argv[1]);
+
+    function_t function = arg_to_function(argv[1]);
+
     g = *argv[1];
     b = *argv[2];
     //  printf("%c\n\n\n", g);
     //  printf("%c\n\n", b);
+
     printf("%s\n\n", argv[1]);
-
-    if (g == 'H')
-    {
-        printf("THIS IS A HELP STATEMENT\n\n");
-        return -1;
-    }
-
-    if (g == 'M') // Choosing type of function
-    {
-        printf("You're multiplying\n");
-        function = 3;
-    }
-    else if (g == 'D')
-    {
-
-        function = 4;
-    }
-    else if (g == 'S')
-    {
-        printf("You're subtracting\n");
-        function = 2;
-    }
-    else if (g == 'A')
-    {
-        printf("You're adding\n");
-        function = 1;
-    }
-    else if (g == 'H')
-    {
-
-        printf("THIS IS A HELP MESSAGE!");
-        debounce = 1;
-    }
-    else
-    {
-        printf("Please enter a valid function type. For a list of accepted functions use --H.\n");
-        debounce = 1;
-        return 1;
-    }
 
     if (debounce == 0)
     {
         printf("A function can proceed!\n\n");
-        if (function == 1)
+        if (function == ADDITION)
         {
             for (i = 2; i < argc; i++)
             {
@@ -79,7 +56,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        else if (function == 2)
+        else if (function == SUBTRACTION)
         {
             char *t = argv[2];
             sumS = strtod(t, &t);
@@ -97,7 +74,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        else if (function == 3)
+        else if (function == MULTIPLICATION)
         {
             for (i = 2; i < argc; i++)
             {
@@ -112,7 +89,7 @@ int main(int argc, char *argv[])
                 // printf("RESULT -------- %lf\n\n", sum);
             }
         }
-        else if (function == 4)
+        else if (function == DIVISION)
         {
             // printf("You're dividing\n");
             char *z = argv[2];
@@ -138,4 +115,51 @@ int main(int argc, char *argv[])
         printf("The Answer is: %lf\n\n", sum);
         return 0;
     }
+}
+
+function_t arg_to_function(char *arg)
+{
+    function_t function = 0;
+
+    if (strncmp(arg, "H", 1) == 0)
+    {
+        printf("THIS IS A HELP STATEMENT\n\n");
+        exit(-1);
+    }
+
+    if (strncmp(arg, "M", 1) == 0) // Choosing type of function
+    {
+        printf("You're multiplying\n");
+        function = MULTIPLICATION;
+    }
+#ifdef TEST
+    else if (arg == 'D')
+    {
+
+        function = 4;
+    }
+
+    else if (arg == 'S')
+    {
+        printf("You're subtracting\n");
+        function = 2;
+    }
+    else if (arg == 'A')
+    {
+        printf("You're adding\n");
+        function = 1;
+    }
+    else if (arg == 'H')
+    {
+
+        printf("THIS IS A HELP MESSAGE!");
+        exit(1);
+    }
+#endif
+    else
+    {
+        printf("Please enter a valid function type. For a list of accepted functions use --H.\n");
+        exit(1);
+    }
+    return function;
 }
